e749084652cd598beb47e95c48326f2fc8760475
Cosmin <cosmin_popica_97@yahoo.com>
2021-04-03 09:47:39 +0300

    Date commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Astazi suntem in data:");
+        System.out.println("    03.04.2021");
     }
 }
